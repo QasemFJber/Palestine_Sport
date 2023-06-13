08af9e3558b2cd2e46767fd30eeb35ffabb38dd5
AbdullahShuhaiber <abdullahahed33@gmail.com>
2023-06-13 11:34:27 +0200

    Abdullah

---
+++ app/src/main/java/com/example/palestinesport/screens/DatasActivity.java
@@ -0,0 +1,15 @@
+package com.example.palestinesport.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatasActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datas);
+    }
+}
