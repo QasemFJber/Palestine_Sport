35a750294c250c27646b8e683adfa130c30a0129
AbdullahShuhaiber <abdullahahed33@gmail.com>
2023-06-13 12:39:58 +0200

    Abdullah

---
+++ app/src/main/java/com/example/palestinesport/MatchScheduleActivity.java
@@ -0,0 +1,14 @@
+package com.example.palestinesport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_schedule);
+    }
+}
