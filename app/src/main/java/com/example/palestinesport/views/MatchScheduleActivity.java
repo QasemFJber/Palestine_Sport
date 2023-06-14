ae8fe1a355689c7f9534fae6a4ab318da1181ff6
jaber <jaberqasem75@gmail.com>
2023-06-14 05:27:47 +0300

    build package For Palestine Sport System

--- app/src/main/java/com/example/palestinesport/views/MatchScheduleActivity.java
+++ app/src/main/java/com/example/palestinesport/views/MatchScheduleActivity.java
@@ -1,8 +1,10 @@
-package com.example.palestinesport;
+package com.example.palestinesport.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.palestinesport.R;
 
 public class MatchScheduleActivity extends AppCompatActivity {
 
