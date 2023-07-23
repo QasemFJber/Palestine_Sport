d7a7f43011e30566a47261bf1ca9c21403f14186
AbdullahShuhaiber <abdullahahed33@gmail.com>
2023-07-23 16:32:17 +0300

    Abdullah

--- app/src/main/java/com/example/palestinesport/views/MatchScheduleActivity.java
+++ app/src/main/java/com/example/palestinesport/views/MatchScheduleActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.palestinesport.R;
@@ -17,8 +18,10 @@ import com.example.palestinesport.interfaces.ItemOnClick;
 import com.example.palestinesport.interfaces.ListCallback;
 import com.example.palestinesport.models.Matchas;
 import com.example.palestinesport.models.Playgound;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MatchScheduleActivity extends AppCompatActivity implements ItemOnClick {
@@ -52,14 +55,30 @@ public class MatchScheduleActivity extends AppCompatActivity implements ItemOnCl
         binding.recMatch.setAdapter(adapter);
         binding.recMatch.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
-
-
         MatchesController controller = new MatchesController();
         controller.getAllMatches(new ListCallback<Matchas>() {
             @Override
             public void onSuccess(List<Matchas> list) {
-                matchas.addAll(list);
-                adapter.notifyDataSetChanged();
+                Toast.makeText(MatchScheduleActivity.this, ""+binding.tabDay.getTabMode(), Toast.LENGTH_SHORT).show();
+                //if ()
+
+                String selectedDay = "السبت";
+                for (Matchas matchas1 : list) {
+                    if (matchas1.matchDay.equals(selectedDay)) {
+                        // تحقق من أن اليوم يتطابق مع اليوم المحدد
+                        // يمكنك استخدام الفرق المتعلقة باليوم المحدد كما تريد
+                        matchas.clear();
+                        Toast.makeText(MatchScheduleActivity.this, ""+Collections.singleton(matchas1).size(), Toast.LENGTH_SHORT).show();
+                        matchas.addAll(Collections.singleton(matchas1));
+                        adapter.notifyDataSetChanged();
+                    }else {
+                        matchas.clear();
+                    }
+
+                }
+
+                //matchas.addAll(list);
+                //adapter.notifyDataSetChanged();
                 //Toast.makeText(MatchScheduleActivity.this, ""+list.size(), Toast.LENGTH_SHORT).show();
             }
 
@@ -69,6 +88,60 @@ public class MatchScheduleActivity extends AppCompatActivity implements ItemOnCl
 
             }
         });
+
+        binding.tabDay.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(MatchScheduleActivity.this, ""+ tab.getText().toString(), Toast.LENGTH_SHORT).show();
+                //tab.getText().toString();
+                matchas.clear();
+
+                controller.getAllMatches(new ListCallback<Matchas>() {
+                    @Override
+                    public void onSuccess(List<Matchas> list) {
+                        //Toast.makeText(MatchScheduleActivity.this, ""+binding.tabDay.getTabMode(), Toast.LENGTH_SHORT).show();
+
+
+                        String selectedDay = tab.getText().toString();
+                        for (Matchas matchas1 : list) {
+                                if (matchas1.matchDay.equals(selectedDay)) {
+                                    // تحقق من أن اليوم يتطابق مع اليوم المحدد
+                                    // يمكنك استخدام الفرق المتعلقة باليوم المحدد كما تريد
+                                    matchas.clear();
+                                    Toast.makeText(MatchScheduleActivity.this, ""+Collections.singleton(matchas1).size(), Toast.LENGTH_SHORT).show();
+                                    matchas.addAll(Collections.singleton(matchas1));
+                                    adapter.notifyDataSetChanged();
+                                }else {
+                                    matchas.clear();
+                                }
+
+                        }
+
+                        //matchas.addAll(list);
+                        //adapter.notifyDataSetChanged();
+
+                        //Toast.makeText(MatchScheduleActivity.this, ""+list.size(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(String message) {
+                        Toast.makeText(MatchScheduleActivity.this, ""+message, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
 
 
     }
